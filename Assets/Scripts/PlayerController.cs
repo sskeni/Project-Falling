cd20c9f24fee4c665c197a0ca450d3d6264771f9
Sanchit <50187427+sskeni@users.noreply.github.com>
2021-07-13 09:47:44 -0700

    Added basic player sprites

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public static PlayerController instance;
 
     private Rigidbody2D Body;
+    private SpriteRenderer SRenderer;
     public float moveSpeed;
     public float jumpSpeed;
     public float pickupBoostSpeed;
@@ -26,6 +27,9 @@ public class PlayerController : MonoBehaviour
     public float milestoneJumpMultiplierChange;
     private float milestoneCounter;
 
+    public Sprite PlayerFalling;
+    public Sprite PlayerFlying;
+
     private void Awake()
     {
         CheckSingleton();
@@ -34,6 +38,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Body = GetComponent<Rigidbody2D>();
+        SRenderer = GetComponent<SpriteRenderer>();
+        SRenderer.sprite = PlayerFalling;
         milestoneCounter = milestoneThreshold;
         groundCheckRadius = 0.2f;
         jumpMultiplier = 1;
@@ -61,6 +67,16 @@ public class PlayerController : MonoBehaviour
             // Move the player
             if (Input.GetMouseButton(0)) Move();
         }
+
+        // Flip player based on movement
+        if (Body.velocity.x < 0)
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, 0f, transform.rotation.z, transform.rotation.w);
+        }
+        if (Body.velocity.x > 0)
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, 180f, transform.rotation.z, transform.rotation.w);
+        }
     }
 
     private void Move()
@@ -72,6 +88,7 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
+        SRenderer.sprite = PlayerFlying;
         Body.velocity = new Vector2(Body.velocity.x, jumpSpeed * jumpMultiplier);
         pickupBoostSpeed = jumpSpeed * jumpMultiplier * 0.5f;
         Body.drag = 1f;
@@ -107,6 +124,7 @@ public class PlayerController : MonoBehaviour
 
     private void GameOver()
     {
+        SRenderer.sprite = PlayerFalling;
         Body.gravityScale = 1f;
         GameManager.instance.OnGameOver();
     }
